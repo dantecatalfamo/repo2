d931bed0491b11a5381081eab60c6531f0e885f3
dantecatalfamo <dante.catalfamo@gmail.com>
2023-07-21 12:54:35 -0400

    Allow custom src root

--- src/env.zig
+++ src/env.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const fs = std.fs;
 const mem = std.mem;
 const debug = std.debug;
 const testing = std.testing;
@@ -8,13 +9,26 @@ pub const EnvironmentValues = struct {
     user: []const u8,
     auth_user: []const u8,
     use_ssh: []const u8,
+    root: []const u8,
 };
 
 pub fn getValues() EnvironmentValues {
+    const root = struct {
+        var buf: [std.os.PATH_MAX]u8 = undefined;
+        var path: []const u8 = "";
+    };
+
+    root.path = blk: {
+        var static_alloc = std.heap.FixedBufferAllocator.init(&root.buf);
+        const home_path = std.os.getenv("HOME") orelse "";
+        break :blk fs.path.join(static_alloc.allocator(), &.{ home_path, "src" }) catch unreachable;
+    };
+
     return .{
         .host = std.os.getenv("REPO_DEFAULT_HOST") orelse "github.com",
         .user = std.os.getenv("REPO_DEFAULT_USER") orelse "dantecatalfamo",
         .auth_user = std.os.getenv("REPO_DEFAULT_AUTH_USER") orelse "git",
         .use_ssh = std.os.getenv("REPO_DEFAULT_USE_SSH") orelse "true",
+        .root = std.os.getenv("REPO_DEFAULT_ROOT") orelse root.path,
     };
 }
