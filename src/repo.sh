8297c930d4098a6f07b7fea387f2256e682db0fb
dantecatalfamo <dante.catalfamo@gmail.com>
2023-11-18 13:56:17 -0500

    Remove unused variables from completion

--- src/repo.sh
+++ src/repo.sh
@@ -34,9 +34,6 @@ function _repo_completions {
     fi
 
     if [ "${#COMP_WORDS[@]}" -eq 3 ] && [ "${COMP_WORDS[1]}" == "cd" ]; then
-        local src_root="$(repo-zig cd)"
-        local only_slashes="${COMP_WORDS[2]//[^\/]}"
-        local num_slashes="${#only_slashes}"
         if [ $num_slashes -lt 2 ]; then
             compopt -o nospace
             COMPREPLY=($(repo cd && compgen -d -S / "${COMP_WORDS[2]}"))
