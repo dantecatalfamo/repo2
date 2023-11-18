98711d21c7c417b43ac239192d62090259bad7d3
dantecatalfamo <dante.catalfamo@gmail.com>
2023-11-18 13:55:00 -0500

    repo cd incremental tab-completion

--- src/repo.sh
+++ src/repo.sh
@@ -26,3 +26,27 @@ function repo-cd {
 function repo-reload {
     eval "$(repo-zig shell)"
 }
+
+function _repo_completions {
+    if [ "${#COMP_WORDS[@]}" -eq 2 ]; then
+        COMPREPLY=($(compgen -W "cd clone help shell env ls new root reload" "${COMP_WORDS[1]}"))
+        return;
+    fi
+
+    if [ "${#COMP_WORDS[@]}" -eq 3 ] && [ "${COMP_WORDS[1]}" == "cd" ]; then
+        local src_root="$(repo-zig cd)"
+        local only_slashes="${COMP_WORDS[2]//[^\/]}"
+        local num_slashes="${#only_slashes}"
+        if [ $num_slashes -lt 2 ]; then
+            compopt -o nospace
+            COMPREPLY=($(repo cd && compgen -d -S / "${COMP_WORDS[2]}"))
+        else
+            COMPREPLY=($(repo cd && compgen -d "${COMP_WORDS[2]}"))
+        fi
+
+    else
+        echo "NOT IT ${COMP_WORDS[1]}"
+    fi
+}
+
+complete -F _repo_completions repo
